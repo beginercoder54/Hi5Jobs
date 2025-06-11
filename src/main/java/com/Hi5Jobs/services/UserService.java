6fc5d4dfd78aed15103d02deb5c3249cfdbf30e9
Phúc Trương <hoangphuc30082004@gmail.com>
2025-06-11 13:58:56 +0700

    valid phone and print if register fail

---
+++ src/main/java/com/Hi5Jobs/services/UserService.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.Hi5Jobs.services;
+
+import com.Hi5Jobs.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ *
+ * @author hoang
+ */
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repo;
+
+    public int checkValidPhone(String Phone){
+        return repo.checkPhoneNumber(Phone);
+    }
+}
